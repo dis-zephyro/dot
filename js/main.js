de23aec66cc6d5ec1c3986846b54018ea357f144
dis-zephyro <zephyro@yandex.ru>
2015-07-15 02:22:06 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.message-theme-inner').jScrollPane({
+        showArrows: true,
+        verticalGutter: 1
+    });
+});
+
+
+$(".teacher-link, .support-link").fancybox({
+    "padding" : 0,
+    "closeBtn" : false
+});
