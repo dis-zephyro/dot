f2a6d2c5d4cbe6cf9e40fb947b2ba30037c13e65
dis-zephyro <zephyro@yandex.ru>
2015-07-17 02:53:22 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ $(document).ready(function() {
 });
 
 
+/* Поле отправки файла */
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }
+    var filename = str.slice(i);
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
+
+
 
 $(".teacher-link, .support-link").fancybox({
     "padding" : 0,
